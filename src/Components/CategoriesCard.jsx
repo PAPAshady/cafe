451b9b7fec748479464775236c27038337ad7424
Nima Zamani <zamani.nima18@gmail.com>
2024-12-16 20:58:04 +0330

    feat(CategoriesCard.jsx): added lazy loading for Category cards images.

--- src/Components/CategoriesCard.jsx
+++ src/Components/CategoriesCard.jsx
@@ -5,7 +5,7 @@ export default function CategoriesCard({ title, img, description }) {
   return (
     <div className="relative flex flex-col border border-primary bg-tertiary p-2 after:absolute after:inset-1/2 after:z-[-1] after:h-[105%] after:w-[90%] after:-translate-x-1/2 after:-translate-y-1/2 after:border after:border-primary-200 after:content-['']">
       <Link to="#">
-        <img className="mx-auto size-[190px] pt-4" src={img} alt="drinks" />
+        <img loading="lazy" className="mx-auto size-[190px] pt-4" src={img} alt="drinks" />
       </Link>
       <div className="px-4 py-6">
         <h2 className="mb-2 text-[1.4rem] font-[600] duration-200 hover:text-primary">
