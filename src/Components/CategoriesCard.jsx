676dff15c9b6a0cf93c5c41f026384b6874d0e79
papashady <zamani.nima18@gmail.com>
2024-12-18 23:31:08 +0330

    feat(categories page): connected backend to categories page to load the dynamic data from server.

--- src/Components/CategoriesCard.jsx
+++ src/Components/CategoriesCard.jsx
@@ -1,17 +1,26 @@
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
+import categoryImg from "../assets/images/categories/categoryImg1.png";
 
-export default function CategoriesCard({ title, img, description }) {
+export default function CategoriesCard({ title, image, description }) {
   return (
     <div className="relative flex flex-col border border-primary bg-tertiary p-2 after:absolute after:inset-1/2 after:z-[-1] after:h-[105%] after:w-[90%] after:-translate-x-1/2 after:-translate-y-1/2 after:border after:border-primary-200 after:content-['']">
       <Link to="#">
-        <img loading="lazy" className="mx-auto size-[190px] pt-4" src={img} alt="drinks" />
+        <img
+          loading="lazy"
+          className="mx-auto size-[190px] pt-4"
+          // since backend currently can't provide image for categories, a static image will be shown for all categories
+          src={image ? image : categoryImg}
+          alt={title}
+        />
       </Link>
       <div className="px-4 py-6">
         <h2 className="mb-2 text-[1.4rem] font-[600] duration-200 hover:text-primary">
           <Link to="#">{title}</Link>
         </h2>
-        <p className="text-sm font-[600] text-quaternary line-clamp-4">{description}</p>
+        <p className="line-clamp-4 text-sm font-[600] text-quaternary">
+          {description}
+        </p>
       </div>
     </div>
   );
@@ -19,6 +28,6 @@ export default function CategoriesCard({ title, img, description }) {
 
 CategoriesCard.propTypes = {
   title: PropTypes.string.isRequired,
-  img: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
 };
--- src/pages/Categories/Categories.jsx
+++ src/pages/Categories/Categories.jsx
@@ -1,57 +1,45 @@
-import CategoryImg1 from "../../assets/images/categories/categoryImg1.png";
-import CategoryImg2 from "../../assets/images/categories/categoryImg2.png";
-import CategoryImg3 from "../../assets/images/categories/categoryImg3.png";
-import CategoryImg4 from "../../assets/images/categories/categoryImg4.png";
+import { useEffect } from "react";
 import { IoFilterSharp } from "react-icons/io5";
 import CategoriesCard from "../../Components/CategoriesCard";
+import useFetch from "../../Hooks/useFetch";
 
 function Categories() {
-  const categories = [
-    {
-      id: 1,
-      title: "Hearty Soups to Warm Your Soul",
-      img: CategoryImg1,
-      description:
-        "Warm up with our hearty soups, made fresh daily with rich flavors and wholesome ingredients, perfect for a cozy meal",
-    },
-    {
-      id: 2,
-      title: "Decadent Desserts for Every Sweet Tooth",
-      img: CategoryImg2,
-      description:
-        "Indulge in our delightful desserts, featuring a variety of sweet treats from decadent cakes to creamy pastries, perfect for satisfying your cravings.",
-    },
-    {
-      id: 3,
-      title: "Fresh and Flavorful Vegan Delights",
-      img: CategoryImg3,
-      description:
-        "Discover our delicious vegan options, crafted with fresh, plant-based ingredients for a wholesome and flavorful dining experience.",
-    },
-    {
-      id: 4,
-      title: "Chilled Refreshments for Every Mood",
-      img: CategoryImg4,
-      description:
-        "Chill out with our refreshing cold beverages, from iced coffees to fruity smoothies and iced teas, perfect for any time of the day.",
-    },
-  ];
+  const {
+    data: categories,
+    loading,
+    error,
+    sendRequest,
+  } = useFetch("categories");
+
+  useEffect(() => {
+    sendRequest();
+  }, []);
+
+  if (error) {
+    return <p>Failed to load categories. Please try again.</p>;
+  }
 
   return (
     <div className="container text-quaternary">
-      <div className="items-center justify-between px-4 sm:flex sm:px-8">
-        <p className="mb-8 text-center sm:m-0">Showing 1–4 of 12 results</p>
-        <button className="mx-auto flex w-[200px] items-center justify-center gap-4 bg-primary p-2 text-lg font-[600] text-white duration-300 hover:bg-secondary sm:m-0">
-          Filters
-          <IoFilterSharp />
-        </button>
-      </div>
+      {loading ? (
+        <span>Loading</span>
+      ) : (
+        <>
+          <div className="items-center justify-between px-4 sm:flex sm:px-8">
+            <p className="mb-8 text-center sm:m-0">Showing 1–4 of 12 results</p>
+            <button className="mx-auto flex w-[200px] items-center justify-center gap-4 bg-primary p-2 text-lg font-[600] text-white duration-300 hover:bg-secondary sm:m-0">
+              Filters
+              <IoFilterSharp />
+            </button>
+          </div>
 
-      <div className="mx-auto mt-2 grid max-w-[400px] grid-cols-1 gap-x-10 gap-y-16 px-2 py-12 sm:max-w-[600px] sm:grid-cols-2 md:max-w-[720px]">
-        {categories.map((category) => (
-          <CategoriesCard key={category.id} {...category} />
-        ))}
-      </div>
+          <div className="mx-auto mt-2 grid max-w-[400px] grid-cols-1 gap-x-10 gap-y-16 px-2 py-12 sm:max-w-[600px] sm:grid-cols-2 md:max-w-[720px]">
+            {categories?.map((category) => (
+              <CategoriesCard key={category._id} {...category} />
+            ))}
+          </div>
+        </>
+      )}
     </div>
   );
 }
