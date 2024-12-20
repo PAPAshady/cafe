a4865659e91b0e0b94e9e874bdf6b99bdac84896
Nima Zamani <zamani.nima18@gmail.com>
2024-12-20 21:59:59 +0330

    added search box input and categories list to the products page also added a range slider which has alot of work to do and is not finished.

--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -1,6 +1,9 @@
 import PropTypes from 'prop-types';
 import productImg from '../../assets/images/categories/categoryImg2.png';
 import ProductCard from '../../Components/ProductCard';
+import { IoSearchOutline, IoChevronForward } from 'react-icons/io5';
+import DualThumbSlider from '../../Components/DualThumbSlider';
+import { Link } from 'react-router-dom';
 
 export default function Products() {
   const productsSortingOptions = [
@@ -33,9 +36,16 @@ export default function Products() {
     },
   ];
 
+  const categoryLinks = [
+    { id: 1, title: 'Deserts', href: '/categories?cat=deserts' },
+    { id: 2, title: 'Healthy Food', href: '/categories?cat=deserts' },
+    { id: 3, title: 'Main Dishes', href: '/categories?cat=main' },
+    { id: 4, title: 'Snacks', href: '/categories?cat=snack' },
+  ];
+
   return (
     <div className="container text-quaternary">
-      <div className="mx-auto flex max-w-[380px] flex-col gap-8 px-2 sm:m-0 sm:max-w-none md:flex-row-reverse md:gap-5 lg:mx-auto lg:max-w-[1024px] xl:m-0 xl:max-w-none">
+      <div className="mx-auto flex max-w-[380px] flex-col gap-x-8 px-2 sm:m-0 sm:max-w-none md:flex-row-reverse md:gap-x-6 lg:mx-auto lg:max-w-[1024px] xl:m-0 xl:max-w-none">
         <div className="md:flex-[3]">
           <div className="flex flex-col items-end sm:flex-row sm:items-center sm:justify-between">
             <p className="mb-8 text-end text-sm font-semibold text-white sm:m-0">
@@ -60,11 +70,55 @@ export default function Products() {
             ))}
           </div>
         </div>
-        <div className="min-h-[300px] bg-slate-600 p-8 text-center text-3xl md:flex-[1.3] lg:flex-1">
-          Filters
+        <div className="flex flex-col gap-12 pt-16 sm:mx-auto sm:w-[80%] md:m-0 md:w-full md:flex-[1.3] lg:flex-1">
+          <div className="flex border-b">
+            <input
+              className="w-full bg-transparent py-3 outline-none"
+              type="text"
+              placeholder="Search Products..."
+            />
+            <button className="p-2">
+              <IoSearchOutline className="size-[25px]" />
+            </button>
+          </div>
+          <div>
+            <p className="mb-6 flex items-center justify-center gap-3 text-xl text-primary">
+              <span className="seperator"></span>
+              <span>Categories</span>
+              <span className="seperator"></span>
+            </p>
+            <ul>
+              {categoryLinks.map((category) => (
+                <CategoryLink key={category.id} {...category} />
+              ))}
+            </ul>
+          </div>
+          <div>
+            <p className="mb-6 flex items-center justify-center gap-3 text-xl text-primary">
+              <span className="seperator"></span>
+              <span>Price Range</span>
+              <span className="seperator"></span>
+            </p>
+
+            <DualThumbSlider />
+          </div>
         </div>
       </div>
     </div>
+  );
+}
+
+function CategoryLink({ href, title }) {
+  return (
+    <li className="mb-4">
+      <Link
+        to={href}
+        className="group flex items-center gap-0.5 text-[1.1rem] text-white transition-all duration-[350ms] hover:gap-1.5 hover:text-primary"
+      >
+        <IoChevronForward className="text-sm transition-colors duration-[350ms] group-hover:text-primary" />
+        {title}
+      </Link>
+    </li>
   );
 }
 
@@ -75,6 +129,11 @@ function ProductsSortBoxOption({ title, value }) {
     </option>
   );
 }
+
+CategoryLink.propTypes = {
+  href: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+};
 
 ProductsSortBoxOption.propTypes = {
   title: PropTypes.string.isRequired,
