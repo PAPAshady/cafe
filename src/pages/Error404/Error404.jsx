d4e79cc0420118220b7e05090d9fe07102903ed7
papashady <zamani.nima18@gmail.com>
2024-12-23 03:31:37 +0330

    fix(Error404.jsx): fixed the bug where the coffee image had too much padding top and the page had no 'container' class.

--- src/pages/Error404/Error404.jsx
+++ src/pages/Error404/Error404.jsx
@@ -4,10 +4,10 @@ import PrimaryBtn from '../../Components/PrimaryBtn/PrimaryBtn';
 export default function Error404() {
   return (
     <>
-      <div className="m-auto leading-none lg:text-[2.3rem]">
+      <div className="m-auto leading-none lg:text-[2.3rem] container">
         <div className="flex items-center justify-center gap-5 text-9xl font-bold leading-none text-white sm:text-[10rem] lg:-mt-6 lg:text-[270px]">
           <p>4</p>
-          <div className="w-24 pt-6 sm:w-28 lg:mb-5 lg:w-44 lg:pt-16">
+          <div className="w-24 pt-3 sm:w-28 lg:mb-5 lg:w-44 lg:pt-16">
             <img src={error404Image} alt="404" className="w-full" />
           </div>
           <p>4</p>
