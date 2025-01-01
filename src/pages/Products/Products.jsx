cf872d5720f3678ef9c2383b83d60953c5c29e97
Nima Zamani <zamani.nima18@gmail.com>
2025-01-01 15:47:20 +0330

    fix(Products.jsx): updated Products page with new icons and layout changes.
    - used proper icon instead of using emojis and characters for rating and buttons in 'Product' component.
    - updated product cards layout and styling for better responsive.

--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -1,8 +1,11 @@
 import { cloneElement, useState } from 'react';
 import { IoSearchOutline, IoTrash } from 'react-icons/io5';
+import { FaStar } from 'react-icons/fa';
+import { FiPlus, FiMinus } from 'react-icons/fi';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { FreeMode } from 'swiper/modules';
 import { categories, products } from '../../data';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import 'swiper/css';
 
@@ -46,7 +49,7 @@ export default function Products() {
       </div>
 
       {/* PRODUCT CARDS */}
-      <div className="px-2 grid w-full grid-cols-2 items-center justify-center gap-5">
+      <div className="grid w-full grid-cols-2 items-center justify-center gap-5 px-2">
         {products.map((product) => (
           <Product product={product} key={product.id} />
         ))}
@@ -67,40 +70,49 @@ function Product({ product }) {
 
   return (
     <div
-      className="relative h-52 w-full overflow-hidden rounded-3xl bg-cover"
+      className="relative h-32 w-full overflow-hidden rounded-3xl bg-cover bg-center min-[400px]:h-40"
       style={{ backgroundImage: `url(${product.img})` }}
     >
-      <div className="absolute z-[1] h-full w-full bg-gradient-to-b from-[rgba(104,104,104,0.14)] from-[50%] to-[rgba(241,146,21,0.77)] to-[100%]"></div>
-      <div className="absolute bottom-0 left-0 z-[2] grid w-full grid-cols-2 items-center justify-between px-3 pb-5 text-white">
-        <h5 className="col-span-2 text-lg">{product.title}</h5>
-        <p className="">{(product.price).toFixed(2)}$</p>
-        {!count ? (
-          <button
-            onClick={increment}
-            className="ml-auto flex size-8 items-center justify-center rounded-full border text-center text-lg"
-          >
-            +
-          </button>
-        ) : (
-          <div className="flex items-center justify-between">
+      <div className="absolute z-[1] h-full w-full bg-gradient-to-b from-[rgba(104,104,104,0.14)] from-[45%] to-[rgba(241,146,21,0.77)] to-[100%]"></div>
+      <div className="absolute bottom-0 left-0 z-[2] w-full items-center justify-between px-3 pb-3 text-white">
+        <Link to="#">
+          <h5 className="mb-0.5 truncate text-base font-semibold min-[400px]:text-lg">
+            {product.title}
+          </h5>
+        </Link>
+        <div className="flex items-center justify-between">
+          <p>{product.price.toFixed(2)}$</p>
+          {!count ? (
             <button
-              className="flex size-8 items-center justify-center rounded-full border text-lg text-white"
-              onClick={decrement}
-            >
-              {count > 1 ? '-' : <IoTrash />}
-            </button>
-            <span className="text-base">{count}</span>
-            <button
-              className="flex size-8 items-center justify-center rounded-full border text-lg text-white"
               onClick={increment}
+              className="flex size-8 items-center justify-center rounded-full border text-center"
             >
-              +
+              <FiPlus />
             </button>
-          </div>
-        )}
+          ) : (
+            <div className="flex items-center justify-between gap-1">
+              <button
+                className="flex size-8 items-center justify-center rounded-full border text-white"
+                onClick={decrement}
+              >
+                {count > 1 ? <FiMinus /> : <IoTrash />}
+              </button>
+              <span className="text-base">{count}</span>
+              <button
+                className="flex size-8 items-center justify-center rounded-full border text-white"
+                onClick={increment}
+              >
+                <FiPlus />
+              </button>
+            </div>
+          )}
+        </div>
       </div>
       <div className="absolute left-3 top-3 font-semibold text-white">
-        <span>⭐{product.rate}</span>
+        <span className="flex items-center gap-1">
+          <FaStar className="text-yellow-400" />
+          {product.rate}
+        </span>
       </div>
     </div>
   );
